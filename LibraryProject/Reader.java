233fdeaeebd00163c638a711d0c1cd2f18fb031b
nguyxnnminh <23010847@st.phenikaa-uni.edu.vn>
2025-05-11 19:06:51 +0700

    Bai tap lon

---
+++ LibraryProject/Reader.java
@@ -0,0 +1,7 @@
+package LibraryProject;
+
+public class Reader extends User {
+    public Reader(String id, String name, String email, String phone) {
+        super(id, name, email, phone);
+    }
+}
